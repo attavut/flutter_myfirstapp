408f6d55bab8d194e14e67543bdf239394676df4
attavut-nb-wealth <attavut.channhu@gmail.com>
2021-02-10 11:29:30 +0700

    add Row Widget & Container Widget

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,10 +8,32 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('My First App'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [Text('Hello World '), Text('Attauvt'), Text('Test')],
+      body: Container(
+        color: Colors.blue[200],
+        //margin: EdgeInsets.all(10),
+        //margin: MediaQuery.of(context).padding, // ถ้าไม่มี appBar จะทำให้เว้นเนื้อที่ด้านบนเผื่อแทบ "สัญญาณ/แบต" ไว้
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Text('Hello World ')],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Text('Attauvt')],
+            ),
+            Padding(
+              //padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Text('WMG')],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
