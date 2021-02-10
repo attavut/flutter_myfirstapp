6495da1a934ee897852e1e1c20090ea618ed657a
attavut-nb-wealth <attavut.channhu@gmail.com>
2021-02-10 17:17:05 +0700

    convert string to json format

--- lib/pages/photo.dart
+++ lib/pages/photo.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,26 +12,30 @@ class PhotoPage extends StatefulWidget {
 
 class _PhotoPageState extends State<StatefulWidget> {
   List<String> list = ['xxx', 'yyy', 'zzz'];
+  var user = {};
 
   @override
   Widget build(BuildContext context) {
     String args = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Photo show'),
-      ),
-      body: Column(
-        // children: list.map((ele) {
-        //   if (ele != 'yyy')
-        //     return Text(ele);
-        //   else
-        //     return Container();
-        // }).toList(),
-        children: list.where((ele) => ele != 'yyy').map((ele) {
-          return Text(ele);
-        }).toList(),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Photo show'),
+        ),
+        body:
+            // Column(
+            //   children: list.where((ele) => ele != 'yyy').map((ele) {
+            //     return Text(ele);
+            //   }).toList(),
+            // ),
+            RaisedButton(
+                child: Text('OK'),
+                onPressed: () {
+                  user['username'] = 'admin';
+                  user['password'] = '1234';
+                  print(user);
+                  String jsonStr = json.encode(user);
+                  print(jsonStr);
+                }));
   }
 }
