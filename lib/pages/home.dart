21b1db8f4b27f2e48f90eb0fa7f5c92cb7afc3e1
attavut-nb-wealth <attavut.channhu@gmail.com>
2021-02-10 12:25:21 +0700

    Add Image, List View, Raised button, Text Field widget

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,36 +5,59 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My First App'),
-      ),
-      body: Container(
-        color: Colors.blue[200],
-        //margin: EdgeInsets.all(10),
-        //margin: MediaQuery.of(context).padding, // ถ้าไม่มี appBar จะทำให้เว้นเนื้อที่ด้านบนเผื่อแทบ "สัญญาณ/แบต" ไว้
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [Text('Hello World ')],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [Text('Attauvt')],
-            ),
-            Padding(
-              //padding: const EdgeInsets.all(8.0),
-              padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [Text('WMG')],
-              ),
-            )
-          ],
+        appBar: AppBar(
+          title: Text('My First App'),
         ),
-      ),
-    );
+        body: Container(
+          // child: Column(
+          //   mainAxisAlignment: MainAxisAlignment.center,
+          child: Center(
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                Image.asset(
+                  'images/logo.jpg',
+                  height: 200,
+                  width: 200,
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(50, 10, 50, 0),
+                  child: TextField(
+                    style: Theme.of(context).textTheme.headline5,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(labelText: 'Username'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(50, 0, 50, 10),
+                  child: TextField(
+                    style: Theme.of(context).textTheme.headline5,
+                    textAlign: TextAlign.center,
+                    obscureText: true,
+                    decoration: InputDecoration(labelText: 'Pasword'),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    RaisedButton(
+                      child: Text("Login"),
+                      onPressed: () {
+                        print('This is Login button');
+                      },
+                    ),
+                    RaisedButton(
+                      child: Text("Register"),
+                      onPressed: () {
+                        print('This is Register button');
+                      },
+                    )
+                  ],
+                ),
+                Center(child: Text("2021 \u00a9 Attavut Channhu")),
+              ],
+            ),
+          ),
+        ));
   }
 }
